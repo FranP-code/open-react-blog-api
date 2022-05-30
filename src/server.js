c9ab15446082e61832993e8ebc0c253273626f60
Francisco Pessano <franpessano1@gmail.com>
2022-05-30 19:19:10 -0300

    Favicon added

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//.env File config
 require('dotenv').config();
 
 const express = require("express");
@@ -5,10 +6,16 @@ const server = express()
 
 const port = process.env.PORT || 3000
 
+//Accept with JSON files
 server.use(express.json())
 
+//Serve public for CSS files
 const path = require("path");
 server.use(express.static(path.resolve('public')));
+
+//Define favicon
+const favicon = require('serve-favicon');
+server.use(favicon(path.resolve('public/images/favicon.ico')));
 
 const index = require('./routes/index.js')
 server.use("/", index)
